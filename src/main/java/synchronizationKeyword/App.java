b0bc07f1b943ef2d43c62804601b2a229332a522
ramninder <ramnindersaini.singh@gmail.com>
2022-06-06 18:18:14 -0400

    multiple locks

--- src/main/java/synchronizationKeyword/App.java
+++ src/main/java/synchronizationKeyword/App.java
@@ -2,7 +2,50 @@ package synchronizationKeyword;
 
 public class App {
 
+    private int count = 0;
     public static void main(String[] args) {
 
+        App app = new App();
+        app.doWork();
     }
+
+
+    public synchronized void increment(){
+        count++;
+    }
+
+
+    public void doWork(){
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0;i<10000;i++){
+                    increment();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0;i<10000;i++){
+                    increment();
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        System.out.println("Count: " + count);
+    }
+
 }
