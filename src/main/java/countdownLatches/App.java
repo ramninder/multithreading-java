179b88eb6445ee5d55a56b69cc4e13b0bc9d7051
ramninder <ramnindersaini.singh@gmail.com>
2022-06-07 18:34:11 -0400

    countdownlatches

---
+++ src/main/java/countdownLatches/App.java
@@ -0,0 +1,52 @@
+package countdownLatches;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//Advanced Java: Multi-threading Part 6 -- Countdown Latches
+public class App {
+
+    public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(3);
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        for(int i=0;i<3;i++) {
+            executor.submit(new Processor(latch));
+        }
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("Completed");
+    }
+
+}
+
+class Processor implements Runnable {
+
+    private CountDownLatch latch;
+
+    public Processor(CountDownLatch latch) {
+        super();
+        this.latch = latch;
+    }
+
+    public void run() {
+        System.out.println("Started.");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        latch.countDown();
+    }
+
+}
+
