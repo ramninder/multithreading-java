d8dc25a9bb8c32b3588422413737a4c3d060c6c2
ramninder <ramnindersaini.singh@gmail.com>
2022-06-08 14:34:08 -0400

    reEntrantLock

---
+++ src/main/java/reEntrantLock/Runner.java
@@ -0,0 +1,51 @@
+package reEntrantLock;
+
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Runner {
+
+    private int count = 0;
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    private void increment() {
+
+        for (int i=0;i<10000;i++) {
+            count++;
+        }
+    }
+    public void firstThread() throws InterruptedException {
+        lock.lock();
+        System.out.println("Waiting.....");
+        condition.await();
+
+        System.out.println("Woken up");
+        try {
+            increment();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void secondThread() throws  InterruptedException{
+        Thread.sleep(5000);
+        lock.lock();
+
+        System.out.println("Press return key!");
+        new Scanner(System.in).nextLine();
+        System.out.println("Got return Key!");
+        condition.signal();
+        try {
+            increment();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void finished() {
+        System.out.println("Count is: "+count);
+    }
+}
